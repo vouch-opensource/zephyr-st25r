2f144c524dd30835e01d7d06fb687a39f4421321
Mike Fikes <mike@fikesfarm.com>
2023-03-23 08:54:16 -0400

    Flesh out a bit of SPI

--- zephyr/src/platform.c
+++ zephyr/src/platform.c
@@ -22,7 +22,13 @@ void platform_st25r_unprotect_comm()
     k_mutex_unlock(&platform_st25r_comm_mutex);
 }
 
-#define DW_INST DT_INST(0, st_st25r)
+#define ST25R_INST DT_INST(0, st_st25r)
+#define ST25R_SPI	DT_PARENT(DT_INST(0, st_st25r))
+
+static const struct device* spi;
+static struct spi_cs_control* cs_ctrl = SPI_CS_CONTROL_PTR_DT(ST25R_INST, 0);
+static struct spi_config spi_cfgs[1] = {0};
+static struct spi_config* spi_cfg;
 
 struct st25r_config {
 	struct gpio_dt_spec gpio_irq;
@@ -33,11 +39,11 @@ struct st25r_config {
 };
 
 static const struct st25r_config conf = {
-	.gpio_irq = GPIO_DT_SPEC_GET_OR(DW_INST, irq_gpios, {0}),
-//	.gpio_reset = GPIO_DT_SPEC_GET_OR(DW_INST, reset_gpios, {0}),
-//	.gpio_wakeup = GPIO_DT_SPEC_GET_OR(DW_INST, wakeup_gpios, {0}),
-//	.gpio_spi_pol = GPIO_DT_SPEC_GET_OR(DW_INST, spi_pol_gpios, {0}),
-//	.gpio_spi_pha = GPIO_DT_SPEC_GET_OR(DW_INST, spi_pha_gpios, {0}),
+	.gpio_irq = GPIO_DT_SPEC_GET_OR(ST25R_INST, irq_gpios, {0}),
+//	.gpio_reset = GPIO_DT_SPEC_GET_OR(ST25R_INST, reset_gpios, {0}),
+//	.gpio_wakeup = GPIO_DT_SPEC_GET_OR(ST25R_INST, wakeup_gpios, {0}),
+//	.gpio_spi_pol = GPIO_DT_SPEC_GET_OR(ST25R_INST, spi_pol_gpios, {0}),
+//	.gpio_spi_pha = GPIO_DT_SPEC_GET_OR(ST25R_INST, spi_pha_gpios, {0}),
 };
 
 gpio_pin_t platform_st25r_int_pin()
@@ -55,15 +61,58 @@ void platform_st25r_gloabl_error(const char* file, long line)
    printk("ST25R error at %s:%ld\n", file, line);
 }
 
-void platform_st25r_spi_transceive(const uint8_t* txBuf, uint8_t* rxBuf, uint16_t len)
+void platform_st25r_spi_transceive(const uint8_t *txBuf, uint8_t *rxBuf, uint16_t len)
 {
-   printk("platform_st25r_spi_transceive %d bytes\n", len);
-}
+    printk("platform_st25r_spi_transceive %d bytes\n", len);
 
+    const struct spi_buf tx_buf[1] = {
+            {
+                    .buf = (void *) txBuf,
+                    .len = len,
+            },
+    };
+    const struct spi_buf_set tx = {
+            .buffers = tx_buf,
+            .count = ARRAY_SIZE(tx_buf),
+    };
+
+    const struct spi_buf rs_buf[1] = {
+            {
+                    .buf = (void *) rxBuf,
+                    .len = len,
+            },
+    };
+    const struct spi_buf_set rx = {
+            .buffers = rx_buf,
+            .count = ARRAY_SIZE(rx_buf),
+    };
+
+    spi_transceive(spi, spi_cfg, &tx, &rx);
+    // TODO check result of spi_tranceive
+}
 
 static int st25r_init(const struct device *dev)
 {
    printk("st25r_init\n");
+
+   /* set common SPI config */
+   for (int i = 0; i < ARRAY_SIZE(spi_cfgs); i++) {
+       spi_cfgs[i].cs = cs_ctrl;
+       spi_cfgs[i].operation = SPI_WORD_SET(8);
+       spi_cfgs[0].frequency = 2000000;
+   }
+
+   spi_cfg = &spi_cfgs[0];
+
+   spi = DEVICE_DT_GET(ST25R_SPI);
+   if (!spi) {
+       LOG_ERR("ST25R SPI binding failed");
+       return -1;
+   } else {
+       LOG_INF("ST25R on %s (max %dMHz)", DT_PROP(DW_INST, label),
+               spi_cfgs[1].frequency / 1000000);
+   }
+   
    rfalNfcInitialize();
    return 0;
 }
