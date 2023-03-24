e8c319c48ceb31117d58e5208cb1849c8c8d0bf3
Mike Fikes <mike@fikesfarm.com>
2023-03-24 15:16:39 -0400

    wip

--- zephyr/src/st25r_spi.c
+++ zephyr/src/st25r_spi.c
@@ -2,13 +2,26 @@
 
 #include <string.h>
 #include "st25r.h"
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 #if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
 
+static const struct device *s_gpiob_dev = DEVICE_DT_GET(DT_NODELABEL(gpiob));
+
 LOG_MODULE_DECLARE(ST25R);
 
 static struct device *s_dev;
+
+static void init_cs_gpio()
+{
+    if (s_gpiob_dev && device_is_ready(s_gpiob_dev)) {
+        gpio_pin_configure(s_gpiob_dev, 6, GPIO_OUTPUT | GPIO_ACTIVE_LOW);
+    } else {
+        LOG_ERR("Unable to access gpiob");
+    }
+}
 
 int st25r_spi_init(const struct device *dev)
 {
@@ -19,6 +32,8 @@ int st25r_spi_init(const struct device *dev)
 		LOG_ERR("Bus device is not ready");
 		return -ENODEV;
 	}
+
+	init_cs_gpio();
 
 	//data->ctx = &st25r_spi_ctx;
 	//data->ctx->handle = (void *)dev;
@@ -31,11 +46,21 @@ int st25r_spi_init(const struct device *dev)
 void platform_st25r_spi_select()
 {
     LOG_INF("SPI select");
+    if (s_gpiob_dev && device_is_ready(s_gpiob_dev)) {
+        gpio_pin_set(s_gpiob_dev, 6, 1);
+    } else {
+        LOG_ERR("Unable to access gpiob");
+    }
 }
 
 void platform_st25r_spi_deselect()
 {
     LOG_INF("SPI deselect");
+    if (s_gpiob_dev && device_is_ready(s_gpiob_dev)) {
+        gpio_pin_set(s_gpiob_dev, 6, 0);
+    } else {
+        LOG_ERR("Unable to access gpiob");
+    }
 }
 
 void platform_st25r_spi_transceive(const uint8_t *txBuf, uint8_t *rxBuf, uint16_t len)
