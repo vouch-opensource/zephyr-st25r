8842c83880bd504b15b6db58c30f89abe1ae2d77
Mike Fikes <mike@fikesfarm.com>
2023-03-24 15:36:18 -0400

    Null the GPIO

--- zephyr/src/st25r_spi.c
+++ zephyr/src/st25r_spi.c
@@ -37,7 +37,7 @@ int st25r_spi_init(const struct device *dev)
 
 	LOG_INF("cs: %p", cs);
 
-	config->spi.config.cs = NULL;
+	config->spi.config.cs.gpio = NULL;
 	init_cs_gpio();
 
 	//data->ctx = &st25r_spi_ctx;
